d7a86fc1be49702eaec5195d75fed247e0d8d66a
AdminPCfix <Admin@DESKTOP-0V9A2T4>
2023-04-03 19:43:12 +0200

    First Push, only 1 AXIS work

---
+++ Assets/Scripts/EnumUsed.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EnumUsed
+{
+   enum names
+    {
+        None, player, deleteOldMaps
+    }
+}
---
+++ Assets/Scripts/MapSpawner2D.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSpawner2D : MonoBehaviour
+{
+    // TO BE UPDATED LATER
+}
